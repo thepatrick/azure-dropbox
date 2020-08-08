5582b4f3c01fe55f9742b1f244e2bcf701be427c
Patrick Quinn-Graham <make-contact@pftqg.com>
2020-08-08 23:10:23 +1000

    Remove fluff

--- backend/begin/index.ts
+++ backend/begin/index.ts
@@ -9,17 +9,6 @@ const containerName = process.env.AZURE_STORAGE_CONTAINER;
 const sanitizeFileNames = (input: string) => input.toLocaleLowerCase().replace(/[^a-z0-9]+/g, '-');
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  context.log('HTTP trigger function processed a request.');
-  // const name = (req.query.name || (req.body && req.body.name));
-  // const responseMessage = name
-  //   ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-  //   : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-  // context.res = {
-  //   // status: 200, /* Defaults to 200 */
-  //   body: responseMessage
-  // };
-
   const version = Date.now();
 
   const objectName = sanitizeFileNames(req.body.speaker) + '-' + sanitizeFileNames(req.body.talk) + '-' + version + extname(req.body.name);
