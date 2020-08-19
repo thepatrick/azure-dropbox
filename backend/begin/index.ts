105cd8fc7b6d1f98efc1f606c7cc3e3d989b8323
Patrick Quinn-Graham <make-contact+930@pftqg.com>
2020-08-19 20:04:03 +1000

    Add room name to filename

--- backend/begin/index.ts
+++ backend/begin/index.ts
@@ -6,12 +6,24 @@ const jwtPrivateKey = process.env.UPLOADER_JWT_PRIVATE_KEY;
 const jwtAudience = process.env.UPLOADER_JWT_AUDIENCE;
 const containerName = process.env.AZURE_STORAGE_CONTAINER;
 
-const sanitizeFileNames = (input: string) => input.toLocaleLowerCase().replace(/[^a-z0-9]+/g, '-');
+const sanitizeFileNames = (input: string) => input.trim().toLocaleLowerCase().replace(/[^a-z0-9]+/g, '-');
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   const version = Date.now();
 
-  const objectName = sanitizeFileNames(req.body.speaker) + '-' + sanitizeFileNames(req.body.talk) + '-' + version + extname(req.body.name);
+  const room = sanitizeFileNames(req.body.room);
+  const speaker = sanitizeFileNames(req.body.speaker);
+  const talk = sanitizeFileNames(req.body.talk);
+
+  if (room.length === 0 || speaker.length === 0 || talk.length === 0 || req.body.name.length === 0) {
+    context.res = {
+      status: 403,
+      body: 'Access Denied'
+    }
+    return;
+  }
+
+  const objectName = `${room}-${speaker}-${talk}-${version}${extname(req.body.name)}`;
 
   context.res = {
     body: JSON.stringify({
